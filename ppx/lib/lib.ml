1fcba950356cd22c0d57f4d1493f6d7a29350b35
Et7f3 <cadeaudeelie@gmail.com>
2020-05-28 19:46:16 +0200

    Initial commit

---
+++ ppx/lib/lib.ml
@@ -0,0 +1,18 @@
+open Migrate_parsetree
+
+open Ast_410
+
+let mapper _config _cookies =
+  let open Ast_mapper in
+  let open Ast_helper in
+  let expr mapper pexp =
+    match pexp.Parsetree.pexp_desc with
+    | Parsetree.Pexp_extension ({txt = "dummy-ppx"; loc=_}, PStr []) -> Exp.constant (Pconst_integer ("42", None))
+    | _ -> default_mapper.expr mapper pexp
+  in
+  {default_mapper with expr}
+
+let () =
+  Migrate_parsetree.Driver.register
+    ~name:"bisect_ppx" ~args:[]
+    Migrate_parsetree.Versions.ocaml_410 mapper
