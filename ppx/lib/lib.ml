e89626708acb1a7a1f47d4166ab1dd9c46f9e657
Et7f3 <cadeaudeelie@gmail.com>
2020-05-28 20:26:49 +0200

    fix: dummy-ppx can't be extension.

--- ppx/lib/lib.ml
+++ ppx/lib/lib.ml
@@ -7,7 +7,7 @@ let mapper _config _cookies =
   let open Ast_helper in
   let expr mapper pexp =
     match pexp.Parsetree.pexp_desc with
-    | Parsetree.Pexp_extension ({txt = "dummy-ppx"; loc=_}, PStr []) -> Exp.constant (Pconst_integer ("42", None))
+    | Parsetree.Pexp_extension ({txt = "dummy_ppx"; loc=_}, PStr []) -> Exp.constant (Pconst_integer ("42", None))
     | _ -> default_mapper.expr mapper pexp
   in
   {default_mapper with expr}
